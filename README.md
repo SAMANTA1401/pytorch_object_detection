2eadd88c489aa744ab6448e3b69df065eb1ec90e
samanta1401 <psamanta1401@gmail.com>
2025-01-02 23:21:06 +0530

    experiment added

---
+++ README.md
@@ -0,0 +1,1 @@
+for image data annotation for objet detection , segmentation : labelImg, labelme, coco-annotator, vggimage annotator, labelbox, roboflow
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="obj_det",
+    version="0.0.1",
+    author="shubhankar",
+    author_email="psamanta1401@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
